f570be083e03f4d4b5fb32c1289a23dc97b10738
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-02 19:32:01 -0300

    Added item to `items` array on button press

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 
+const items = [];
+
 function App() {
   const [item, setItem] = useState("");
+
   return (
     <div className="container">
       <div className="heading">
@@ -12,7 +15,8 @@ function App() {
           value={item}
           onChange={event => setItem(event.target.value)}
         />
-        <button>
+        <button
+          onClick={() => { items.push(item); setItem(""); console.log(items)}}>
           <span>Add</span>
         </button>
       </div>
