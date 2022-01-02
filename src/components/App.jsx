f237c5791c0a419fa76d54d62564509089a24222
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-02 19:22:25 -0300

    Saved item state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text"
+          value={item}
+          onChange={event => setItem(event.target.value)}
+        />
         <button>
           <span>Add</span>
         </button>
